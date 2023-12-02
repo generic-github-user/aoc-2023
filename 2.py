82025799afe85ddd575814f97acbb07449be651f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-12-01 21:41:43 -0500

    add solution for puzzle 2

---
+++ 2.py
@@ -0,0 +1,7 @@
+with open('input-1.txt', 'r') as file:
+    digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]\
+            + list(map(str, range(1, 10)))
+    # unfortunate
+    f = lambda s, g, h: str(digits.index(g([d for d in digits if d in s], key=h)) % 9 + 1)
+    print(sum(int(f(s, min, s.find) + f(s, max, s.rfind)) for s in file))
+    # for s in file: print(s, f(s, min, s.find), f(s, max, s.rfind))
