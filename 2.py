69341ae5ea589bd54ab04c3f53ff99114dc941e0
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-12-02 01:13:43 -0500

    simplify

--- 2.py
+++ 2.py
@@ -2,5 +2,5 @@ with open('input-1.txt', 'r') as file:
     digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]\
             + list(map(str, range(1, 10)))
     # unfortunate
-    f = lambda s, g, h: str(digits.index(g([d for d in digits if d in s], key=h)) % 9 + 1)
-    print(sum(int(f(s, min, s.find) + f(s, max, s.rfind)) for s in file))
+    f = lambda s, g, h: digits.index(g([d for d in digits if d in s], key=h)) % 9 + 1
+    print(sum(f(s, min, s.find) * 10 + f(s, max, s.rfind) for s in file))
