a774ad38163ed586b213f19761215dbba18c2699
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-12-02 19:01:47 -0500

    add puzzle 3 solution in Haskell

---
+++ 3.hs
@@ -0,0 +1,17 @@
+import Data.List.Split
+import qualified Data.Map.Strict as M
+
+parseColor :: String -> (String, Integer)
+parseColor c = (x !! 1, read $ x !! 0)
+    where x = splitOn " " c
+filterGames :: [String] -> String -> Integer
+filterGames games bag =
+    sum $ map (read . head . (splitOn ":") . (drop 5))
+        (filter (\g -> all (\r -> all
+            (\c -> let c2 = parseColor c in
+                (snd c2) <= (maybe (-1) id (M.lookup (fst c2) b))) (splitOn ", " r))
+            (splitOn "; " $ last $ splitOn ": " g)) games)
+        where b = M.fromList (map parseColor (splitOn ", " bag))
+main = do
+    content <- readFile "./input-2.txt"
+    print (filterGames (lines content) "12 red, 13 green, 14 blue")
