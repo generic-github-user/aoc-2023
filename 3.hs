36ad598b7ace6967122a840f16a5f93a39eccd40
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2023-12-02 19:25:50 -0500

    revise puzzle 3 solution

--- 3.hs
+++ 3.hs
@@ -4,14 +4,16 @@ import qualified Data.Map.Strict as M
 parseColor :: String -> (String, Integer)
 parseColor c = (x !! 1, read $ x !! 0)
     where x = splitOn " " c
+
 filterGames :: [String] -> String -> Integer
 filterGames games bag =
     sum $ map (read . head . (splitOn ":") . (drop 5))
         (filter (\g -> all (\r -> all
             (\c -> let c2 = parseColor c in
-                (snd c2) <= (maybe (-1) id (M.lookup (fst c2) b))) (splitOn ", " r))
+                (snd c2) <= (b M.! (fst c2))) (splitOn ", " r))
             (splitOn "; " $ last $ splitOn ": " g)) games)
         where b = M.fromList (map parseColor (splitOn ", " bag))
+
 main = do
     content <- readFile "./input-2.txt"
     print (filterGames (lines content) "12 red, 13 green, 14 blue")
